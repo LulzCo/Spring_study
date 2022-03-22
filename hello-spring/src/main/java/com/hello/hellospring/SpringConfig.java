cb211e0b9200e1c015e463f9037f1d7c583fc2af
seongwon-gitcodes <ysw991106@gmail.com>
2022-03-22 17:00:22 +0900

    update spring db access tech that include original jdbc

--- hello-spring/src/main/java/com/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/com/hello/hellospring/SpringConfig.java
@@ -1,21 +1,30 @@
 package com.hello.hellospring;
 
+import com.hello.hellospring.repository.JdbcMemberRepository;
 import com.hello.hellospring.repository.MemberRepository;
 import com.hello.hellospring.repository.MemoryMemberRepository;
 import com.hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//public class SpringConfig {
-//
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository());
-//    }
-//
-//    @Bean
-//    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//    }
-//}
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JdbcMemberRepository(dataSource);
+    }
+}
