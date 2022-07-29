20859ef426946093c66d53c660afb27d424e23e0
seongwon-gitcodes <ysw991106@gmail.com>
2022-07-29 17:41:11 +0900

    PutController 생성

---
+++ SpringBootGuide/api/src/main/java/com/springboot/api/controller/PutController.java
@@ -0,0 +1,30 @@
+package com.springboot.api.controller;
+
+import com.springboot.api.dto.MemberDto;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/api/v1/put-api")
+public class PutController {
+
+    @PutMapping(value = "/member")
+    public String putMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PutMapping(value = "/member2")
+    public String putMemberDto(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+}
