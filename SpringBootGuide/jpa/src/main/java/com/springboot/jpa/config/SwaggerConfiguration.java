2cf8d71221c3de666cf8cdd8373e854ad023af44
seongwon-gitcodes <ysw991106@gmail.com>
2022-08-12 21:14:53 +0900

    jpa project

--- SpringBootGuide/jpa/src/main/java/com/springboot/jpa/config/SwaggerConfiguration.java
+++ SpringBootGuide/jpa/src/main/java/com/springboot/jpa/config/SwaggerConfiguration.java
@@ -19,7 +19,7 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.springboot.jap"))
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.jpa"))
                 .paths(PathSelectors.any())
                 .build();
     }
