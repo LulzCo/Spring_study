eb99eaa0ea7136d32822a6c5cb099a0d2093e055
seongwon-gitcodes <ysw991106@gmail.com>
2022-08-15 17:31:13 +0900

    PutController updating and DeleteController creating

--- gradle/src/main/java/com/gradleboot/gradle/controller/apiController/PutController.java
+++ gradle/src/main/java/com/gradleboot/gradle/controller/apiController/PutController.java
@@ -1,6 +1,8 @@
 package com.gradleboot.gradle.controller.apiController;
 
 import com.gradleboot.gradle.data.member.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +29,19 @@ public class PutController {
     }
 
     @PutMapping(value = "/member1")
-    public MemberDto putMemberDto1(@RequestBody MemberDto memberDto) {
+    public String putMemberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
+    @PutMapping(value = "/member2")
+    public MemberDto putMemberDto(@RequestBody MemberDto memberDto) {
         return memberDto;
+    }
+
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> putMemberDto3(@RequestBody MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
     }
 }
