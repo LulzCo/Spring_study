1ee4622935c79458070ade73190fced90ed74294
seongwon-gitcodes <ysw991106@gmail.com>
2022-08-18 23:30:10 +0900

    ProductDaoImpl update

--- gradle/src/main/java/com/gradleboot/gradle/data/product/dao/impl/ProductDaoImpl.java
+++ gradle/src/main/java/com/gradleboot/gradle/data/product/dao/impl/ProductDaoImpl.java
@@ -1,9 +1,40 @@
 package com.gradleboot.gradle.data.product.dao.impl;
 
+import com.gradleboot.gradle.data.product.dao.ProductDao;
+import com.gradleboot.gradle.data.product.entity.Product;
+import com.gradleboot.gradle.data.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ProductDaoImpl {
+public class ProductDaoImpl implements ProductDao {
 
+    private ProductRepository productRepository;
 
+    @Autowired
+    public ProductDaoImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product insertProduct(Product product) {
+        Product saveProduct = productRepository.save(product);
+
+        return saveProduct;
+    }
+
+    @Override
+    public Product selectProduct(Long number) {
+        return null;
+    }
+
+    @Override
+    public Product updateProductName(Long number, String name) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteProdict(Long number) throws Exception {
+
+    }
 }
--- gradle/src/main/java/com/gradleboot/gradle/data/product/repository/ProductRepository.java
+++ gradle/src/main/java/com/gradleboot/gradle/data/product/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.gradleboot.gradle.data.product.repository;
 
+import com.gradleboot.gradle.data.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
