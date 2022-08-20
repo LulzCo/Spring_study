e556f7e6ea75c45b89cbf46d4c6d414d4e66c6e4
seongwon-gitcodes <ysw991106@gmail.com>
2022-08-21 01:52:42 +0900

    ProductController

--- gradle/src/main/java/com/gradleboot/gradle/controller/productController/ProductController.java
+++ gradle/src/main/java/com/gradleboot/gradle/controller/productController/ProductController.java
@@ -1,8 +1,52 @@
 package com.gradleboot.gradle.controller.productController;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.gradleboot.gradle.data.product.dto.ChangeProductNameDto;
+import com.gradleboot.gradle.data.product.dto.ProductDto;
+import com.gradleboot.gradle.data.product.dto.ProductResponseDto;
+import com.gradleboot.gradle.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ProductResponseDto> getProduct(Long number) {
+
+        ProductResponseDto productResponseDto = productService.getProduct(number);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductDto productDto) {
+
+        ProductResponseDto productResponseDto = productService.saveProduct(productDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+    
+    @PutMapping
+    public ResponseEntity<ProductResponseDto> changeProductName(@RequestBody ChangeProductNameDto changeProductNameDto) throws Exception {
+
+        ProductResponseDto productResponseDto = productService.changeProductName(changeProductNameDto.getNumber(), changeProductNameDto.getName());
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteProduct(Long number) throws Exception {
+
+        productService.deleteProduct(number);
+
+        return ResponseEntity.status(HttpStatus.OK).body("삭제 완료");
+    }
 
 }
