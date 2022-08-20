2e6699b032dcc23aa281997215568867073ea81b
seongwon-gitcodes <ysw991106@gmail.com>
2022-08-20 20:58:26 +0900

    move HelloController

--- gradle/src/main/java/com/gradleboot/gradle/controller/helloController/HelloController.java
+++ gradle/src/main/java/com/gradleboot/gradle/controller/helloController/HelloController.java
@@ -1,4 +1,4 @@
-package com.gradleboot.gradle.controller;
+package com.gradleboot.gradle.controller.helloController;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
