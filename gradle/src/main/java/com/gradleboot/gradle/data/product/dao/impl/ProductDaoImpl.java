c5666d129b270bd1c95b81ecd9828e159ec1362d
seongwon-gitcodes <ysw991106@gmail.com>
2022-08-23 18:21:28 +0900

    환경설정 오류 해결 및 DAO 오류 해결

--- gradle/src/main/java/com/gradleboot/gradle/data/product/dao/impl/ProductDaoImpl.java
+++ gradle/src/main/java/com/gradleboot/gradle/data/product/dao/impl/ProductDaoImpl.java
@@ -6,6 +6,9 @@ import com.gradleboot.gradle.data.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Component
 public class ProductDaoImpl implements ProductDao {
 
@@ -25,16 +28,42 @@ public class ProductDaoImpl implements ProductDao {
 
     @Override
     public Product selectProduct(Long number) {
-        return null;
+        Product selectedProduct = productRepository.getById(number);
+
+        return selectedProduct;
     }
 
     @Override
     public Product updateProductName(Long number, String name) throws Exception {
-        return null;
+        Optional<Product> selectedProduct = productRepository.findById(number);
+
+        Product updatedProduct;
+
+        if (selectedProduct.isPresent()) {
+            Product product = selectedProduct.get();
+
+            product.setName(name);
+            product.setUpdatedAt(LocalDateTime.now());
+
+            updatedProduct = productRepository.save(product);
+        } else {
+            throw new Exception();
+        }
+
+        return updatedProduct;
+
     }
 
     @Override
     public void deleteProduct(Long number) throws Exception {
+        Optional<Product> selectedProduct = productRepository.findById(number);
 
+        if (selectedProduct.isPresent()) {
+            Product product = selectedProduct.get();
+
+            productRepository.delete(product);
+        } else {
+            throw new Exception();
+        }
     }
 }
