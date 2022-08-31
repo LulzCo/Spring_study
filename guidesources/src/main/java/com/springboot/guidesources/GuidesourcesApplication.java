8ba1ceebe130d4faeb4487bd33ae384a8b2d8d98
seongwon-gitcodes <ysw991106@gmail.com>
2022-08-31 16:39:05 +0900

    new Project

---
+++ guidesources/src/main/java/com/springboot/guidesources/GuidesourcesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.guidesources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidesourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuidesourcesApplication.class, args);
+	}
+
+}
---
+++ guidesources/src/test/java/com/springboot/guidesources/GuidesourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.guidesources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidesourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
