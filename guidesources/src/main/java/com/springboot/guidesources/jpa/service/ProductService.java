ebecf2b7a40bfa0b28510a58c3db410092ee0d12
seongwon-gitcodes <ysw991106@gmail.com>
2022-09-03 16:40:10 +0900

    Product Service 생성

---
+++ guidesources/src/main/java/com/springboot/guidesources/jpa/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.springboot.guidesources.jpa.service;
+
+import com.springboot.guidesources.jpa.data.dto.ProductDto;
+import com.springboot.guidesources.jpa.data.dto.ProductResponseDto;
+
+public interface ProductService {
+
+    ProductResponseDto getProduct(Long number);
+
+    ProductResponseDto saveProduct(ProductDto productDto);
+
+    ProductResponseDto changeProductName(Long number, String name);
+
+    void deleteProduct(Long number) throws Exception;
+}
