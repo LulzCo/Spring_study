0c53af35479588f4cf823ea28529bccfb583eeca
seongwon-gitcodes <ysw991106@gmail.com>
2022-09-03 17:49:06 +0900

    ProductController 생성

---
+++ guidesources/src/main/java/com/springboot/guidesources/jpa/controller/ProductController.java
@@ -0,0 +1,52 @@
+package com.springboot.guidesources.jpa.controller;
+
+import com.springboot.guidesources.jpa.data.dto.ProductDto;
+import com.springboot.guidesources.jpa.data.dto.ProductResponseDto;
+import com.springboot.guidesources.jpa.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ProductResponseDto> getProduct(Long number) {
+
+        ProductResponseDto productResponseDto = productService.getProduct(number);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponseDto> saveProduct(ProductDto productDto) {
+
+        ProductResponseDto productResponseDto = productService.saveProduct(productDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @PutMapping
+    public ResponseEntity<ProductResponseDto> changeProductName(Long number, String name) throws Exception {
+
+        ProductResponseDto productResponseDto = productService.changeProductName(number, name);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDto);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteProduct(Long number) throws Exception {
+        productService.deleteProduct(number);
+
+        return ResponseEntity.status(HttpStatus.OK).body("삭제 완료");
+    }
+}
