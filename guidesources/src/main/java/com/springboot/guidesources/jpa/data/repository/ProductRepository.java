2ff8c6ba295d43372ec1e2348127453b405eb715
seongwon-gitcodes <ysw991106@gmail.com>
2022-09-03 15:19:17 +0900

    ProductRepostitory 생성

---
+++ guidesources/src/main/java/com/springboot/guidesources/jpa/data/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.springboot.guidesources.jpa.data.repository;
+
+import com.springboot.guidesources.jpa.data.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
+z
