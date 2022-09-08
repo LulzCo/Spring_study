9fb453165028c5fdbe1de7a09d78d0f127da0e7a
seongwon-gitcodes <ysw991106@gmail.com>
2022-09-08 17:51:11 +0900

    db 연결 수정

--- guidesources/src/main/java/com/springboot/guidesources/jpa/data/entity/Product.java
+++ guidesources/src/main/java/com/springboot/guidesources/jpa/data/entity/Product.java
@@ -1,12 +1,15 @@
 package com.springboot.guidesources.jpa.data.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Table(name = "product")
 public class Product {
 
