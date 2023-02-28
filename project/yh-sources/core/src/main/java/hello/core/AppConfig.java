454cc6678a9a90cc28d384d9d53a8d12ea50e859
LulzCo <ysw991106@gmail.com>
2023-02-28 19:11:33 +0900

    app config 역할 부여

--- project/yh-sources/core/src/main/java/hello/core/AppConfig.java
+++ project/yh-sources/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,11 +13,18 @@ public class AppConfig {
 
     public MemberService memberService() {
         // 생성자 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    private DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
 }
