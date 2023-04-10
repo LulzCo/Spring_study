0cf0661e6cd0921732fb86406e0996658fc83ec2
LulzCo <ysw991106@gmail.com>
2023-04-10 19:36:52 +0900

    scope 프록시모드를 통한 코드 최적화 및 주의사항

--- project/yh-sources/core/src/main/java/hello/core/common/MyLogger.java
+++ project/yh-sources/core/src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,11 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")           // 단 하나의 요청 내에서만 동작한다!! 단!!! 하!나!의 요청!!       // 주석처리해서 실행을 하면 모든 요청이 같은 uuid를 가지며 실행되고 있다!!!!!
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)           // 단 하나의 요청 내에서만 동작한다!! 단!!! 하!나!의 요청!!       // 주석처리해서 실행을 하면 모든 요청이 같은 uuid를 가지며 실행되고 있다!!!!!
+/*ScopedProxyMode.TARGET_CLASS
+* -> 프로바이더처럼 동작하게 만들기 위해 가짜 프록시 객체를 만들어 사용, 스프링 컨테이너에 MyLogger를 빈으로 등록하지 않고 가짜 프록시 객체를 만들어 등록
+* -> 스프링 컨테이너에 껍데기인 프록시 객체를 등록하고 나서 실제로 호출되고 사용될 때 진짜 객체를 등록한다. -> 마치 싱글톤 처럼 사용한다!!(실제 싱글톤이라 생각하면 안된다!!) -> 무분별하게 사용해서는 안된다!!
+* 프록시 객체 덕분에 다형성과 DI 컨테이너 가능*/
 public class MyLogger {
 
     private String uuid;
